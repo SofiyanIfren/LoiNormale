6b5a8abbcafc974de90c0d89891ab22bbb218558
Sofiyan-MT <sifren@maitrise-technologique.com>
2019-08-06 15:44:03 +0200

    first commit

---
+++ Algorithmes.java
@@ -0,0 +1,48 @@
+package mt.utilitaires;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Algorithmes {
+	
+	/**
+	 * @author Sofiyan Ifren
+	 * @return Retourne une liste de valeurs épurée au-delà et en-deça de lécart-type (loi normale)
+	 */
+	public List<Long> gaussianFunction (List<Long> listeDonnees){
+		List<Long> listeFinale = new ArrayList<Long>();
+		// Utilisation de cette fonction uniquement si la liste n'est pas vide
+		if (!listeDonnees.isEmpty()) {
+			// 1. Obtention de la moyenne
+			long total = 0;
+			for (long data : listeDonnees) {
+				total += data;
+			}
+			long moyenne = total / listeDonnees.size();
+			
+			// 2. Calcul de la variance
+			long variance = 0;
+			long sommeDesCarres = 0;
+			for (long data : listeDonnees) {
+				// mise au carré de chaque opération (Xi - moyenne)
+				long carreDesSommes = (data-moyenne)*(data-moyenne);
+				sommeDesCarres += carreDesSommes;
+			}
+			variance = sommeDesCarres / (listeDonnees.size()-1);
+			
+			// 3. Calcul de l'écart-type
+			long ecartType = (long) Math.sqrt(variance);
+			long valeurMax = moyenne + ecartType;
+			long valeurMin = moyenne - ecartType;
+			
+			// 4. Tri des valeurs de la liste initiale
+			for (long data : listeDonnees) {
+				if (data < valeurMax && data > valeurMin){
+					listeFinale.add(data);
+				}
+			}
+		}
+		return listeFinale;
+	}
+
+}
---
+++ TimeMethods.java
@@ -0,0 +1,34 @@
+﻿package mt.utilitaires;
+
+import java.util.Date;
+
+public class TimeMethods {
+		
+	/**
+	 * @author Sofiyan Ifren
+	 * @return Retourne la date minimale entre deux temps donnés (Date)
+	 */
+	public Date getMinimalDate(Date date1, Date date2){
+		Date dateFinale = null;
+		if (date1.after(date2)){
+			dateFinale = date2;
+		} else {
+			dateFinale = date1;
+		}	
+		return dateFinale;	
+	}
+	/**
+	 * @author Sofiyan Ifren
+	 * @return Retourne la date maximale entre deux temps donnés (Date)
+	 */
+	public Date getMaximalDate(Date date1, Date date2){
+		Date dateFinale = null;
+		if (date1.before(date2)){
+			dateFinale = date2;
+		} else {
+			dateFinale = date1;
+		}	
+		return dateFinale;
+	}
+	
+}
